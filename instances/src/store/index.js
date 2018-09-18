bc129e7411849deb938509787249cd62b617fce3
Szecowka, Adam <adam.szecowka@gmail.com>
2018-09-18 10:24:28 +0200

    si websockets

--- instances/src/store/index.js
+++ instances/src/store/index.js
@@ -1,4 +1,4 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import builder from './../commons/builder';
 import resolvers from './resolvers';
@@ -8,6 +8,7 @@ import { split, ApolloLink, concat } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { WebSocketLink } from 'apollo-link-ws';
 import { getMainDefinition } from 'apollo-utilities';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 
 export function createApolloClient() {
   const cache = new InMemoryCache();
@@ -15,7 +16,8 @@ export function createApolloClient() {
     process.env.REACT_APP_LOCAL_API ? 'graphqlApiUrlLocal' : 'graphqlApiUrl',
   );
 
-  const wsUrl = getURL('subscriptionsApiUrl');
+  //  const wsUrl = getURL('subscriptionsApiUrl');
+  const wsUrl = 'wss://ui-api.kyma.local/graphql/subscriptions';
 
   // Create an http link:
   const httpLink = new HttpLink({
@@ -27,43 +29,49 @@ export function createApolloClient() {
     uri: wsUrl,
     options: {
       reconnect: true,
+      connectionParams: () => {
+        console.log('connection params ', builder.getBearerToken());
+        return {
+          authToken: builder.getBearerToken() || null,
+        };
+      },
     },
   });
 
-  const authMiddleware = new ApolloLink((operation, forward) => {
-    // add the authorization to the headers
-    console.log('token', builder.getBearerToken());
-    operation.setContext(({ headers = {} }) => ({
-      headers: {
-        ...headers,
-        authorization: builder.getBearerToken() || null,
-      },
-    }));
-    return forward(operation);
-  });
+  // const authMiddleware = new ApolloLink((operation, forward) => {
+  //     // add the authorization to the headers
+  //     console.log('token', builder.getBearerToken());
+  //     operation.setContext(({headers = {}}) => ({
+  //         headers: {
+  //             ...headers,
+  //             authorization: builder.getBearerToken() || null,
+  //         },
+  //     }));
+  //     return forward(operation);
+  // });
 
-  const link = split(
+  const link2 = split(
     // split based on operation type
     ({ query }) => {
-      console.log('Link', query);
-      //const { kind, operation } = getMainDefinition(query);
-      //return kind === 'OperationDefinition' && operation === 'subscription';
-      return true;
+      const { kind, operation } = getMainDefinition(query);
+      let v = kind === 'OperationDefinition' && operation === 'subscription';
+      console.log('Link splitting', v, query);
     },
-    httpLink,
     wsLink,
+    httpLink,
   );
 
   const client = new ApolloClient({
-    link: concat(authMiddleware, link),
-    // request: async operation => {
-    //   operation.setContext(({ headers = {} }) => ({
-    //     headers: {
-    //       ...headers,
-    //       authorization: builder.getBearerToken() || null,
-    //     },
-    //   }));
-    // },
+    link: link2,
+
+    request: async operation => {
+      operation.setContext(({ headers = {} }) => ({
+        headers: {
+          ...headers,
+          authorization: builder.getBearerToken() || null,
+        },
+      }));
+    },
     cache: cache,
     onError: ({ graphQLErrors, networkError }) => {
       if (graphQLErrors) {
